64ac29a5fcd5f5ffc0ef1c56031a2620e8b7cce5
Abhishek Nere <abhisheksandeepnere@gmail.com>
2026-04-25 15:38:12 +0530

    Started with my new Project : UrbanStay - Backend, The AirBnb clone

---
+++ src/main/java/com/abhi/projects/airBnbApp/entity/Hotel.java
@@ -0,0 +1,27 @@
+package com.abhi.projects.airBnbApp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "hotel")
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String city;
+
+    @Column(columnDefinition = "TEXT[]")
+    private String[] photos;
+
+    @Column(columnDefinition = "TEXT[]")
+    private String[] amenities;
+}
